44d64c6d3f498bce6873dcadf3c5ff8bb45d293d
Pedro Padilla Quesada <pquesada@fuelcellsystems.co.uk>
2025-08-07 16:18:19 +0100

    Updates __init__ files.

--- sammoo/__init__.py
+++ sammoo/__init__.py
@@ -1,6 +1,12 @@
 from .config_selection import ConfigSelection
 from .parmoo_simulation import ParMOOSim
 from .components.thermal_load_lpg import ThermalLoadProfileLPG
+from .components.solar_loop_configuration import SolarLoopConfiguration
 from .version import __version__
 
-__all__ = ["ConfigSelection", "ParMOOSim", "ThermalLoadProfileLPG"]
+__all__ = [
+    "ConfigSelection",
+    "ParMOOSim",
+    "ThermalLoadProfileLPG",
+    "SolarLoopConfiguration",
+    ]
--- sammoo/components/__init__.py
+++ sammoo/components/__init__.py
@@ -1,3 +1,6 @@
 # sammoo/profiles/__init__.py
 
 from .thermal_load_lpg import ThermalLoadProfileLPG
+from .solar_loop_configuration import SolarLoopConfiguration
+
+__all__ = ["ThermalLoadProfileLPG", "SolarLoopConfiguration"]
